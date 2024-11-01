8bc9cae0177b54118ecbfff4ba73e40878df312f
renzito0637d <renzito0637d@gmail.com>
2024-10-31 23:53:29 -0500

    in progress

--- src/Controller/BasicProcesses.java
+++ src/Controller/BasicProcesses.java
@@ -1,9 +1,14 @@
 package Controller;
 
+import View.UI_Basic;
 /**
  *
  * @author Renzo
  */
 public class BasicProcesses {
-    
+    public static void Presentation(UI_Basic bc){
+        bc.setTitle("Calculator for GG");
+        bc.setLocationRelativeTo(null);        
+        bc.setVisible(true);
+    }
 }
